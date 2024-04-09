56dd7fd50fc35d43b6af290ee02a971396eabd8d
Pathak1511 <hritik1511@gmail.com>
2024-04-09 20:20:47 +0530

    20:30:08/04

--- src/pages/profile/index.jsx
+++ src/pages/profile/index.jsx
@@ -1,7 +1,7 @@
 import Image from "next/image";
 import { Inter } from "next/font/google";
 import { useState } from "react";
-import CircularImg from "../../components/CircularImg";
+import CircularImg from "@/components/Cards";
 const inter = Inter({ subsets: ["latin"] });
 import { useRouter } from "next/router";
 
